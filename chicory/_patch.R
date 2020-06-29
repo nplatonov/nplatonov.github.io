77982808fb0694c7aaebc0e3eca0dca01d93c058
nplatonov <nplatonov@yahoo.com>
2020-06-29 22:27:02 +0300

    ongoing

---
+++ chicory/_patch.R
@@ -0,0 +1,24 @@
+list1 <- dir(pattern="^r.+\\.html")
+if (F) {
+   res <- unname(sapply(sample(list1),function(fname) {
+     # message(fname)
+      a <- readLines(fname)
+      ind <- grep("<title>",a)
+      gsub("(<|>|/|title|ArcNet PAC|\\s+)","",a[ind])
+   }))
+   print(sort(as.integer(res)))
+}
+if (T) {
+   res <- unname(sapply(sample(list1),function(fname) {
+      message(fname)
+      a <- readLines(fname)
+      ind <- grep("220px",a)
+      if (length(ind)) {
+        # print(a[ind])
+         a[ind] <- gsub("220px","210px",a[ind])
+        # print(a[ind])
+      }
+      writeLines(a,fname)
+     # gsub("(<|>|/|title|ArcNet PAC|\\s+)","",a[ind])
+   }))
+}
