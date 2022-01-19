49ebf37fb981f8d1506c00aad8fe261bb87ee17c
nplatonov <nplatonov@yahoo.com>
2022-01-19 11:03:41 +0300

    ongoing

--- _download.R
+++ _download.R
@@ -1,6 +1,9 @@
 if (T) {
    ret <- c(pull=999)
    ret[1] <- system("git pull")
+   if (ret[1]) {
+      Sys.sleep(35)
+   }
   # if (!(ret[2] <- system("git commit -m\"ongoing\"")))
   #    ret[3] <- system("git push")
    ret
