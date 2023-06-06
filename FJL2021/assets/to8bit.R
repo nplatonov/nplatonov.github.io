f72e9311ab7053277015bbc2973aed2001da24af
nplatonov <nplatonov@yahoo.com>
2023-06-06 12:35:01 +0300

    ongoing

--- FJL2021/assets/to8bit.R
+++
@@ -1,6 +0,0 @@
-list1 <- dir(pattern="\\.png$",recursive=TRUE,full.names=TRUE)
-ret <- lapply(sample(list1),function(x) {
-   src <- normalizePath(x)
-   message(src)
-   system2("i_view32",c(src,"/bpp=8",paste0("/convert=",dQuote(src))))
-})
