142bcdca114f61e97923d33ea46b3f2eb19aee14
nplatonov <nplatonov@yahoo.com>
2024-04-26 11:43:41 +0300

    ongoing

--- sevinGIS/Полина Ильина/hw3/reproduce.bat
+++
@@ -1,3 +0,0 @@
-::glance "заприпайные полыньи.tif" height=1600 col=green coast.lwd=2 pointsize=36 fileout="заприпайные полыньи.png" bpp=8
-glance полынья.shp field=area blank=white coast.fill=#00000010 fileout=полынья.png bpp=8
-::glance "полынья 2.shp" field=area blank=white coast.fill=#00000010 fileout="полынья 2.png" bpp=8
