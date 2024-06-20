d7f2ea5f9521a3a058ccb2550672dd14233d0ff8
nplatonov <nplatonov@yahoo.com>
2024-06-20 12:34:36 +0300

    ongoing

--- site_libs/moon_reader_after.js
+++
@@ -1,5 +0,0 @@
-if( 'querySelectorAll' in document ) {
-   $('.bigtext').bigtext();
-}
-// $('.remark-slide-content').fitText(1.2, { minFontSize: '14px', maxFontSize: '136px' });
-$('.fitText').fitText(1.2, { minFontSize: '14px', maxFontSize: '936px' });
