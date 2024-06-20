fa99d4bd6044d278490448f80f789d99195ddb20
nplatonov <nplatonov@yahoo.com>
2024-06-20 12:33:55 +0300

    ongoing

---
+++ moon_reader-after.js
@@ -0,0 +1,5 @@
+if( 'querySelectorAll' in document ) {
+   $('.bigtext').bigtext();
+}
+// $('.remark-slide-content').fitText(1.2, { minFontSize: '14px', maxFontSize: '136px' });
+$('.fitText').fitText(1.2, { minFontSize: '14px', maxFontSize: '936px' });
