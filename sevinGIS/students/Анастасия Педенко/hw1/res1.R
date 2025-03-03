6dfa403ebce163ca5b891d74669b594bd6df1f8b
nplatonov <nplatonov@yahoo.com>
2025-03-03 16:14:58 +0300

    ongoing

--- sevinGIS/students/Анастасия Педенко/hw1/res1.R
+++
@@ -1,9 +0,0 @@
-plutil::ursula(3)
-pt1 <- spatialize("Волхов")
-pt2 <- spatialize("Киркенес")
-pt <- spatial_bind(pt1,pt2)
-glance(pt,resetGrid=TRUE,style="Positron",basemap.order="before",dim=c(750,400),scalebar.w=75)
-
-
-
-
