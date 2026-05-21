4bd58ab41ec11570d5d4584734e69efcbb68c9c7
nplatonov <nplatonov@yahoo.com>
2026-05-21 18:36:55 +0300

    ongoing

--- sevinGIS/students/Ксения Савельева/hw4/reproduced.R
+++
@@ -1,4 +0,0 @@
-require(ursa)
-pol <- spatial_read("Полыньи.shz")
-glance(pol[,grep("(name|area)",spatial_colnames(pol),ignore.case=TRUE)]
-      ,resetGrid=TRUE,border=0,coast.col="black",legend=list("left","right"))
